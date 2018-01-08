206fc92e76e313240415b3ac68b906b8b8a4afad
unknown <Administrator@ZHAO-HUI.jiangtai.com>
2018-01-08 17:03:21 +0800

    “commit”

--- teaching/run_tests.py
+++ teaching/run_tests.py
@@ -1,19 +1,16 @@
 #coding=utf8
 
 from HTMLTestRunner import HTMLTestRunner
-import unittest,time,os
+import unittest,time
 from setting.send_email import *
 
-base_dir = os.path.dirname(os.path.dirname(__file__)) # 返回当前文件所在目录的上级目录
 
-foot= base_dir + "/teaching/number/foot"  #找到excel数据配置文件
-print foot
 discover=unittest.defaultTestLoader.discover("./case",pattern="foot1.py")
 
 if __name__=="__main__":
     runner = unittest.TextTestRunner()
     runner.run(discover)
-    sed_mail(foot)  # 发送邮件
+    sed_mail("E:\JK-JT\workspace\Teaching and insurance network\teaching\number\foot.txt")  # 发送邮件
 
 
 
